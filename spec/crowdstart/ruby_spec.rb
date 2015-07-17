fb8d82d1f919c2b7759bfb608a37a5cd944767fc
David Tai <david@verus.io>
2015-07-17 16:06:23 -0700

    fixed rspec

--- spec/crowdstart/ruby_spec.rb
+++ spec/crowdstart/ruby_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
-describe Crowdstart::Ruby do
+describe Crowdstart::Rails do
   it 'has a version number' do
-    expect(Crowdstart::Ruby::VERSION).not_to be nil
+    expect(Crowdstart::Rails::VERSION).not_to be nil
   end
 
   it 'does something useful' do
